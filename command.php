6fe5318be681110540ede5cf5e48b9b7229270ee
Sara <sarapearce3.14@gmail.com>
2018-02-15 03:54:37 -0600

    fixed tab alignment

--- command.php
+++ command.php
@@ -23,127 +23,129 @@ if ( ! class_exists( ' WP_CLI ' ) ) {
 	return;
 }
 
-if ( ! class_exists( 'CXL_Command' ) ) {
+// if ( ! class_exists( 'CXL_Command' ) ) {
 	/**
 	 * This class builds an Intercom object that allows us to CRUD on the Intercom server,
 	 * then it removes duplicate leads on the Intercom remote server.
 	 */
-	class CXL_Command {
+class CXL_Command {
+		// print('INSIDE THE CLASS');
 		/**
 		 * Get the token and build the Intercom object
-		 * $args is an array of arguments, currently empty
+		 *
+		 * @args is an array of arguments, currently empty
 		 */
-		public function __invoke( $args ) {
-			echo 'HERE';
-			WP_CLI::success( $args[0] );
+	public function __invoke( $args ) {
+		// echo 'INVOKE';
+		WP_CLI::success( $args[0] );
 
-			/**
-			 * Build the Intercom object
-			 */
+		/**
+		 * Build the Intercom object
+		 */
 
-			$token = $this->get_token();
-			try {
-				$this->intercom = new IntercomClient( $token, null );
+		$token = $this->get_token();
+		try {
+			$this->intercom = new IntercomClient( $token, null );
 
-			} catch ( Exception $e ) {
-				echo 'Unable to create Intercom Object ',  '\n';
-				echo ' Caught exception:  ' ,  esc_url( $e->getMessage() ), ' \n ';
-				die();
-			}
-			$this->remove_lead_dupes();
-
-		// $this->reset_test_data();
-
+		} catch ( Exception $e ) {
+			echo 'Unable to create Intercom Object ',  '\n';
+			echo ' Caught exception:  ' ,  esc_url( $e->getMessage() ), ' \n ';
+			die();
 		}
+		$this->remove_lead_dupes();
+
+	// $this->reset_test_data();
+
+	}
 
 			/**
 			 * Delete the duplicate leads using the Intercom object
 			 */
-		public function remove_lead_dupes() {
-			/**
-			 * Identify what leads are the duplicates, and get a list of their ids
-			 */
-			$duplicate_leads = $this->find_duplicate_leads( $this->intercom->leads->getLeads( [] ) );
+	public function remove_lead_dupes() {
+		/**
+		 * Identify what leads are the duplicates, and get a list of their ids
+		 */
+		$duplicate_leads = $this->find_duplicate_leads( $this->intercom->leads->getLeads( [] ) );
 
-			/**
-			 * Remove the duplicate leads from Intercom
-			 */
-			$success = $this->remove_duplicate_leads( $duplicate_leads );
-		}
+		/**
+		 * Remove the duplicate leads from Intercom
+		 */
+		$success = $this->remove_duplicate_leads( $duplicate_leads );
+	}
 
 			/**
 			 * Get the Intercom token from the database
 			 */
-		private function get_token() {
-			return get_option( ' intercom-token ' );
-		}
+	private function get_token() {
+		return get_option( ' intercom-token ' );
+	}
 
 			/**
 			 * Used for testing. It generates 2 users with the email of sarapearce3.14@gmail.com
 			 */
-		private function reset_test_data() {
-			/**
-			 * Create the same user twice to create a duplicate
-			 */
-			$this->intercom->leads->create( [ ' email ' => ' sarapearce3.14@gmail.com ' ] );
-			$this->intercom->leads->create( [ ' email ' => ' sarapearce3.14@gmail.com ' ] );
+	private function reset_test_data() {
+		/**
+		 * Create the same user twice to create a duplicate
+		 */
+		$this->intercom->leads->create( [ ' email ' => ' sarapearce3.14@gmail.com ' ] );
+		$this->intercom->leads->create( [ ' email ' => ' sarapearce3.14@gmail.com ' ] );
 
-			echo 'Data Reset!';
-		}
+		echo 'Data Reset!';
+	}
 			/**
 			 * Sort and isolate the duplicate leads using the email as the unique identifier.
 			 *
-			 * $leads is an object with contacts and metadata
+			 * @$leads is an object with contacts and metadata
 			 */
-		public function find_duplicate_leads( $leads ) {
-			$unique_leads = [];
-			$duplicate_leads = [];
+	public function find_duplicate_leads( $leads ) {
+		$unique_leads = [];
+		$duplicate_leads = [];
 
-			foreach ( $leads->contacts as $key => $lead ) {
-				/**
-				 * Use an email as an indicator of a unique lead
-				 */
-				if ( ! in_array( $leads->contacts[ $key ]->email, $unique_leads, true) ) {
-					array_push( $unique_leads, $leads->contacts[ $key ]->email );
-				} else {
-					array_push( $duplicate_leads, $leads->contacts[ $key ]->id );
-				}
-			}
-
+		foreach ( $leads->contacts as $key => $lead ) {
 			/**
-			 * Confirm we havent left behind any dupes
+			 * Use an email as an indicator of a unique lead
 			 */
-			if ( array_unique( $unique_leads ) ) {
-				return $duplicate_leads;
+			if ( ! in_array( $leads->contacts[ $key ]->email, $unique_leads, true) ) {
+				array_push( $unique_leads, $leads->contacts[ $key ]->email );
 			} else {
-				echo ' the unique leads array still has dupes ';
+				array_push( $duplicate_leads, $leads->contacts[ $key ]->id );
 			}
 		}
 
 		/**
-		 * Delete the duplicate leads using the Intercom object
-		 *
-		 * $duplicate_leads is an array of ids where each is the id for the lead that is a duplicate
+		 * Confirm we havent left behind any dupes
 		 */
-		public function remove_duplicate_leads( $duplicate_leads ) {
-			if ( empty( $duplicate_leads ) ) {
-				return false;
-			}
-			foreach ( $duplicate_leads as $key => $lead ) {
-				try {
-					$this->intercom->leads->deleteLead( $lead );
-				} catch ( Exception $e ) {
-					echo ' Unable to delete duplicate leads ' ,  ' \n ';
-					echo ' Caught exception:  ' ,  esc_url( $e->getMessage() ),'\n';
-					return false;
-				}
-			}
-			return true;
+		if ( array_unique( $unique_leads ) ) {
+			return $duplicate_leads;
+		} else {
+			echo ' the unique leads array still has dupes ';
 		}
 	}
+
+		/**
+		 * Delete the duplicate leads using the Intercom object
+		 *
+		 * @$duplicate_leads is an array of ids where each is the id for the lead that is a duplicate
+		 */
+	public function remove_duplicate_leads( $duplicate_leads ) {
+		if ( empty( $duplicate_leads ) ) {
+			return false;
+		}
+		foreach ( $duplicate_leads as $key => $lead ) {
+			try {
+				$this->intercom->leads->deleteLead( $lead );
+			} catch ( Exception $e ) {
+				echo ' Unable to delete duplicate leads ' ,  ' \n ';
+				echo ' Caught exception:  ' ,  esc_url( $e->getMessage() ),'\n';
+				return false;
+			}
+		}
+		return true;
+	}
+}
 
 	/**
 		* Add the function to the command object
 		*/
-	WP_CLI::add_command( 'cxl-intercom', 'CXL_Command' );
-}
+WP_CLI::add_command( 'cxl-intercom', 'CXL_Command' );
+// }
